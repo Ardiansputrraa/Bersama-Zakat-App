6d5257e7726e23978c603e2951583cc029cf3403
Alan <anugrahlanpambudi27@gmail.com>
2023-11-01 16:45:43 +0700

    zakatfitrah

---
+++ app/src/main/java/com/example/bersamazakatapp/ui/zakat_fitrah/ZakatFitrahFragment.kt
@@ -0,0 +1,62 @@
+package com.example.bersamazakatapp.ui.zakat_emas
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import com.example.bersamazakatapp.R
+import com.example.bersamazakatapp.adapter.ViewPagerAdapter
+import com.example.bersamazakatapp.databinding.FragmentZakatEmasBinding
+import com.example.bersamazakatapp.databinding.FragmentZakatFitrahBinding
+import com.example.bersamazakatapp.konten.PengertianFragment
+import com.example.bersamazakatapp.konten.RefrensiPandanganFragment
+import com.example.bersamazakatapp.konten.SyaratFragment
+import com.example.bersamazakatapp.konten.TataCaraFragment
+import com.google.android.material.bottomsheet.BottomSheetDialog
+
+class ZakatFitrahFragment : Fragment() {
+
+    private var _zakatFitrahBinding : FragmentZakatFitrahBinding? = null
+    private val zakatFitrahBinding get() = _zakatFitrahBinding!!
+    private lateinit var adapterViewPager : ViewPagerAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _zakatFitrahBinding = FragmentZakatFitrahBinding.inflate(inflater,container,false)
+        val view = zakatFitrahBinding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        super.onViewCreated(view, savedInstanceState)
+        _zakatFitrahBinding = FragmentZakatFitrahBinding.bind(view)
+
+        zakatFitrahBinding.buttonHitungZakatFitrah.setOnClickListener{
+            val viewDialog : View = layoutInflater.inflate(R.layout.bottom_sheet_dialog,null)
+            val dialog = BottomSheetDialog(requireContext())
+            dialog.setContentView(viewDialog)
+            dialog.show()
+
+//            val imageButtonCloseBottomSheetDialog = dialog.findViewById<Button>(R.id.imageButtonCloseBottomSheetDialog)
+//            imageButtonCloseBottomSheetDialog?.setOnClickListener {
+//                dialog.dismiss()
+//            }
+        }
+
+        adapterViewPager = ViewPagerAdapter(requireActivity().supportFragmentManager)
+        adapterViewPager.addFragment(PengertianFragment(), "Pengertian")
+        adapterViewPager.addFragment(SyaratFragment(), "Syarat")
+        adapterViewPager.addFragment(TataCaraFragment(), "Tata Cara")
+        adapterViewPager.addFragment(RefrensiPandanganFragment(), "Refrensi Pandangan")
+
+
+//        zakatFitrahBinding.viewpagerZakatFitrah.adapter = adapterViewPager
+//        zakatFitrahBinding.tablayoutZakatFitrah.setupWithViewPager(zakatFitrahBinding.viewpagerZakatFitrah)
+
+    }
+}
